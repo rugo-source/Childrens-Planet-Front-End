d3f9ab81c7e53f13e8e517c3c49c6a3c2e7a42ec
rugo-source <rugo@live.com.mx>
2021-05-01 16:37:51 -0500

    added body

--- pages/index.js
+++ pages/index.js
@@ -1,83 +1,76 @@
-import Head from "next/head";
+import { Container,Row,Col,Card,Image, Form, Button} from 'react-bootstrap';
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <body className={styles.body}>
-      <main className="d-flex align-items-center min-vh-100 py-3 py-md-0">
-        <div className="container">
-          <div className={styles.logincard}>
-            <div className="row no-gutters">
-              <div className="col-md-5">
-                <img src="/login.jpg" alt="login" className={styles.loginimg} />
-              </div>
-              <div className="col-md-7">
-                <div className={styles.cardbody}>
+    <Container >
+      <main className="d-flex align-items-center min-vh-100 p-4 py-md-0">
+        <Container>
+          <Card className={styles.logincard}>
+            <Row className="no-gutters">
+              <Col md={5}>
+                <Image src="/login.jpg" alt="login" className={styles.loginimg} />
+              </Col>
+              <Col md={7}>
+                <Card.Body className={styles.logincardbody}>
                   <div className={styles.brandwrapper}>
-                    <img
+                    <Image
                       src="/logo.svg"
                       alt="logo"
                       className={styles.brandwrapper}
                     />
                   </div>
-                  <p className={styles.logincarddescription}>
+                  <Card.Text className={styles.logincarddescription}>
                     Sign into your account
-                  </p>
-                  <form action="#!">
-                    <div className={styles.formgroup}>
-                      <label htmlFor="email" className="sr-only">
+                  </Card.Text>
+                  <Form >
+                    <Form.Group className={styles.formgroup}>
+                      <Form.Label htmlFor="email" className="sr-only">
                         Email
-                      </label>
-                      <input
+                      </Form.Label>
+                      <Form.Control
                         type="email"
                         name="email"
                         id="email"
-                        className={styles.formgroup}
+                        calassName={styles.formgroup}
                         placeholder="Email address"
                       />
-                    </div>
-                    <div className={styles.formgroup} md={4}>
-                      <label htmlFor="password" className="sr-only">
+                    </Form.Group>
+                    <Form.Group className={styles.formgroup} md={4}>
+                      <Form.Label htmlFor="password" className="sr-only">
                         Password
-                      </label>
-                      <input
+                      </Form.Label>
+                      <Form.Control
                         type="password"
                         name="password"
                         id="password"
-                        className="form-control"
+                        className={styles.formgroup}
                         placeholder="***********"
                       />
-                    </div>
-                    <input
+                    </Form.Group>
+                    <Form.Control
+                    
                       name="login"
                       id="login"
-                      //buton
                       className={styles.loginbtn}
                       mb={4}
-                      type="button"
+                      type="submit"
                       value="Login"
                     />
-                  </form>
-                  <a href="#!" className={styles.forgotpasswordlink}>
-                    Forgot password?
-                  </a>
-                  <p className={styles.logincardfootertext}>
+                  </Form>
+                  <Card.Text className={styles.logincardfootertext}>
                     Don't have an account?
-                    <a href="#!" className="text-reset">
+                    <Card.Link href="#!" className="text-reset">
                       Register here
-                    </a>
-                  </p>
-                  <nav className="login-card-footer-nav">
-                    <a href="#!">Terms of use.</a>
-                    <a href="#!">Privacy policy</a>
-                  </nav>
-                </div>
-              </div>
-              <div className="col-md-7"></div>
-            </div>
-          </div>
-        </div>
+                    </Card.Link>
+                  </Card.Text>
+                 
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+        </Container>
       </main>
-    </body>
+    </Container>
   );
 }
