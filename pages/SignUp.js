38afb2bd095c4a605ef3b54e1cacf81a267b43b7
braulio0 <braulioelias6@gmail.com>
2021-05-02 16:56:36 -0500

    added functions at singUp

--- pages/SignUp.js
+++ pages/SignUp.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   Container,
   Row,
@@ -8,84 +8,161 @@ import {
   Form,
   Button,
 } from "react-bootstrap";
+import NavBar from "../components/Navbar";
 import DatePicker from "react-datepicker";
+import { Countries } from "../constants/info";
 import "react-datepicker/dist/react-datepicker.css";
 const SignUp = () => {
-    const [startDate, setStartDate] = useState(new Date());
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    address: "",
+    city: "",
+    state: "",
+    zip: 0,
+  });
+  const [startDate, setStartDate] = useState(new Date());
+  const handleChange = (event) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(data);
+  };
+
+  /*useEffect(() => {
+    const handleChange = (event) => {
+      setData({
+        ...date,
+        startDate,
+      });
+    };
+  }, [startDate]);
+  */
   return (
-    <div className="main">
-      <Card>
-        <Container className="d-flex justify-content-center">
-          <Card.Body>
-            <Card.Title>
-              <h2 className="form-title">Create account</h2>
-            </Card.Title>
-            <Form>
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridFirstName">
-                  <Form.Label>First name</Form.Label>
-                  <Form.Control type="fistname" placeholder="First name" />
+    <>
+      <NavBar />
+      <Container className="d-flex justify-content-center p-4 py-0">
+        <Row className="p-4 py-0">
+          <Card className="p-4 py-0">
+            <Card.Body>
+              <Card.Title>
+                <h2 className="form-title">Create account</h2>
+              </Card.Title>
+              <Form onSubmit={handleSubmit}>
+                <Form.Row>
+                  <Form.Group as={Col} controlId="formGridFirstName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                      type="name"
+                      placeholder="First name"
+                      required
+                      name="name"
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                </Form.Row>
+
+                <Form.Row>
+                  <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      placeholder="Enter email"
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+
+                  <Form.Group as={Col} controlId="formGridPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      required
+                      onChange={handleChange}
+                      min={8}
+                    />
+                  </Form.Group>
+                </Form.Row>
+
+                <Form.Label
+                  className="my-1 mr-2"
+                  htmlFor="inlineFormCustomSelectPref"
+                >
+                  Birthday
+                </Form.Label>
+                <Form.Group>
+                  <DatePicker
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                  />
+                </Form.Group>
+                <Form.Group controlId="formGridAddress1">
+                  <Form.Label>Address </Form.Label>
+                  <Form.Control
+                    name="address"
+                    placeholder="1234 Main St"
+                    required
+                    onChange={handleChange}
+                  />
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridLastName">
-                  <Form.Label>Last name</Form.Label>
-                  <Form.Control type="lastname" placeholder="Last Name" />
-                </Form.Group>
-              </Form.Row>
+                <Form.Row>
+                  <Form.Group as={Col} controlId="formGridCity">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control name="city" onChange={handleChange} />
+                  </Form.Group>
 
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
+                  <Form.Group as={Col} controlId="formGridState">
+                    <Form.Label>State</Form.Label>
+                    <Form.Control
+                      as="select"
+                      name="state"
+                      onChange={handleChange}
+                    >
+                      {Countries.map((countries) =>
+                        countries === "" ? (
+                          <option key={countries} value={countries}>
+                            --Seleccione una--
+                          </option>
+                        ) : (
+                          <option
+                            key={countries}
+                            name="state"
+                            value={countries}
+                          >
+                            {countries}
+                          </option>
+                        )
+                      )}
+                    </Form.Control>
+                  </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-              </Form.Row>
+                  <Form.Group as={Col} controlId="formGridZip">
+                    <Form.Label>Zip</Form.Label>
+                    <Form.Control
+                      type="number"
+                      name="zip"
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                </Form.Row>
 
-              <Form.Label
-                className="my-1 mr-2"
-                htmlFor="inlineFormCustomSelectPref"
-              >
-              Birthday
-              </Form.Label>
-              <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
-
-              <Form.Group controlId="formGridAddress1">
-                <Form.Label>Address </Form.Label>
-                <Form.Control placeholder="1234 Main St" />
-              </Form.Group>
-
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridCity">
-                  <Form.Label>City</Form.Label>
-                  <Form.Control />
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridState">
-                  <Form.Label>State</Form.Label>
-                  <Form.Control as="select" defaultValue="Choose...">
-                    <option>Choose...</option>
-                    <option>...</option>
-                  </Form.Control>
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridZip">
-                  <Form.Label>Zip</Form.Label>
-                  <Form.Control />
-                </Form.Group>
-              </Form.Row>
-
-              <Button variant="primary" type="submit">
-                Register
-              </Button>
-            </Form>
-          </Card.Body>
-        </Container>
-      </Card>
-    </div>
+                <Button variant="primary" type="submit">
+                  Register
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
+    </>
   );
 };
 export default SignUp;
