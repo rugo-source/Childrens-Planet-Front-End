3bb7525b68a60ce500e50ecdedaae815b13eb7e1
rugo-source <rugo@live.com.mx>
2021-05-05 14:53:31 -0500

    added registrer resrvation

--- pages/SignUpReservation.js
+++ pages/SignUpReservation.js
@@ -14,86 +14,98 @@ import NavBar from "../components/Navbar";
 import DatePicker from "react-datepicker";
 import { Countries } from "../constants/info";
 import "react-datepicker/dist/react-datepicker.css";
+import setHours from "date-fns/setHours";
+import setMinutes from "date-fns/setMinutes";
 const SignUpReservation = () => {
   const [startDate, setStartDate] = useState(new Date());
+  const [Hora, setHora] = useState(setHours(setMinutes(new Date(), 0), 16));
   const handleChange = (event) => {
     setData({
       ...data,
       [event.target.name]: event.target.value,
     });
   };
-    return (
-      <>
+  const isWeekday = date => {
+    const day = getDay(date);
+    return day !== 0 && day !== 7;
+  };
+
+  return (
+    <>
+    <NavBar/>
       <Container className="d-flex justify-content-center p-4 py-0">
-      <Row className="p-4 py-0">
-        <Card className="p-4 py-0">
-          <Card.Body>
-            <Card.Title>
-              <h2 className="form-title">Create Reservation</h2>
-            </Card.Title>
-            <Form >
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridFirstName">
-                  <Form.Label>Horario</Form.Label>
-                  <Form.Control
-                    type="name"
-                    placeholder="First name"
-                    required
-                    name="name"
-                    onChange={handleChange}
+        <Row className="p-4 py-0">
+          <Card className="p-4 py-0">
+            <Card.Body>
+              <Card.Title>
+                <h2 className="form-title">Create Reservation</h2>
+              </Card.Title>
+              <Form>
+                <Form.Row>
+                  <Form.Group as={Col} name="hora">
+                    <Form.Label
+                      className="my-1 mr-2"
+                      htmlFor="inlineFormCustomSelectPref"
+                    >
+                      Horario{" "}
+                    </Form.Label>
+                    <DatePicker
+                      selected={Hora}
+                      onChange={(date) => setHora(date)}
+                      showTimeSelect
+                      showTimeSelectOnly
+                      timeIntervals={60}
+                      timeCaption="Time"
+                      includeTimes={[
+                        setHours(setMinutes(new Date(), 0), 10),
+                        setHours(setMinutes(new Date(), 0), 11),
+                        setHours(setMinutes(new Date(), 0), 12),
+                        setHours(setMinutes(new Date(), 0), 13),
+                        setHours(setMinutes(new Date(), 0), 14),
+                        setHours(setMinutes(new Date(), 0), 15),
+                        setHours(setMinutes(new Date(), 0), 16),
+                        setHours(setMinutes(new Date(), 0), 17),
+                        setHours(setMinutes(new Date(), 0), 18),
+                      ]}
+                      dateFormat="h:mm aa"
+                    />
+                  </Form.Group>
+                </Form.Row>
+
+                <Form.Row>
+                  <Form.Group as={Col} name="fecha">
+                    <Form.Label>Fecha</Form.Label>
+                    <DatePicker
+                      selected={startDate}
+                      onChange={(date) => setStartDate(date)}
+                    />
+                  </Form.Group>
+                </Form.Row>
+
+                <Form.Label
+                  className="my-1 mr-2"
+                  htmlFor="inlineFormCustomSelectPref"
+                >
+                  Fecha
+                </Form.Label>
+                <Form.Group name="date">
+                  <DatePicker
+                    dateFormat="yyyy/MM/dd"
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    filterDate={isWeekday}
                   />
                 </Form.Group>
-              </Form.Row>
 
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control
-                    type="email"
-                    name="email"
-                    placeholder="Enter email"
-                    onChange={handleChange}
-                  />
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    required
-                    onChange={handleChange}
-                    min={8}
-                  />
-                </Form.Group>
-              </Form.Row>
-
-              <Form.Label
-                className="my-1 mr-2"
-                htmlFor="inlineFormCustomSelectPref"
-              >
-                Birthday
-              </Form.Label>
-              <Form.Group name="date">
-                <DatePicker
-                  dateFormat="yyyy/MM/dd"
-                  selected={startDate}
-                  withPortal
-                  onChange={date => setStartDate(date)}
-                />
-              </Form.Group>
-
-              <Button variant="primary" type="submit">
-                Register
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </Row>
-    </Container>
-  </>
- 
-    );
- };
- export default SignUpReservation;
+                <Button variant="primary" type="submit">
+                  Register
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
+    </>
+  );
+};
+export default SignUpReservation;
