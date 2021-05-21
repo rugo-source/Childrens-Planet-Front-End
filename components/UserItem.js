3bdae1f92146304c488246b4c3d1a0f913cfbf6e
rugo-source <rugo@live.com.mx>
2021-05-21 15:17:11 -0500

    added something

--- components/UserItem.js
+++ components/UserItem.js
@@ -1,8 +1,21 @@
 import { Button } from "react-bootstrap";
+import { State,useState } from "react";
+import axios from "axios";
 const UserItem = ({ name, domicilio, email, age }) => {
   const handleSubmit = () => {
     console.log(email);
+
+    axios
+      .post(`http://localhost:8080/users/delete`,data)
+      .then((res) => {
+        res.send(data);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        //useState error
+      });
   };
+
 
   return (
     <>
